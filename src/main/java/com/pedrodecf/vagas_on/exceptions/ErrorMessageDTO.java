f4349ca5ef7e51ae7a9bdf05f70061fafe788c14
pedrodecf <pedro012014@gmail.com>
2024-07-30 00:03:41 -0300

    feat: error validation

---
+++ src/main/java/com/pedrodecf/vagas_on/exceptions/ErrorMessageDTO.java
@@ -0,0 +1,11 @@
+package com.pedrodecf.vagas_on.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessageDTO {
+    private String message;
+    private String field;
+}
--- src/main/java/com/pedrodecf/vagas_on/modules/candidate/CandidateEntity.java
+++ src/main/java/com/pedrodecf/vagas_on/modules/candidate/CandidateEntity.java
@@ -1,6 +1,9 @@
 package com.pedrodecf.vagas_on.modules.candidate;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import java.util.UUID;
 
@@ -9,9 +12,16 @@ public class CandidateEntity {
 
     private UUID id;
     private String name;
+
+    @Pattern(regexp = "\\S+", message = "Username should contain only letters and numbers")
     private String username;
+
+    @Email(message = "Email should be valid and contain '@' and '.'")
     private String email;
+
+    @Length(min = 6, message = "Password should have at least 6 characters", max = 100)
     private String password;
+
     private String description;
     private String curriculum;
 }
